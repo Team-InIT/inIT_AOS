69b8580205efdda8c2f6a576d10ffde4ae731ceb
lhb8106 <lhb8106@naver.com>
2022-04-15 15:56:48 +0900

    [FEAT] setting end

--- app/src/main/java/com/init_android/app/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/init_android/app/presentation/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.init_android
+package com.init_android.app.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.init_android.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
