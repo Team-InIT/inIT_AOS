bbfbb95c1c0c7c88951fe431c17a499c0c679c64
stopkite <62979643+stopkite@users.noreply.github.com>
2022-05-09 01:08:44 +0900

    [FEAT] #5 - add signup first fragment

--- app/src/main/java/com/init_android/app/presentation/ui/home/signup/BaseInfoOneFragment.kt
+++ app/src/main/java/com/init_android/app/presentation/ui/home/signup/BaseInfoOneFragment.kt
@@ -1,5 +1,7 @@
 package com.init_android.app.presentation.ui.home.signup
 
+import android.os.Bundle
+import android.view.View
 import com.init_android.R
 import com.init_android.databinding.FragmentBaseInfoOneBinding
 import com.playtogether_android.app.presentation.base.BaseFragment
@@ -7,4 +9,21 @@ import com.playtogether_android.app.presentation.base.BaseFragment
 // 기본정보(1)
 class BaseInfoOneFragment:BaseFragment<FragmentBaseInfoOneBinding>(R.layout.fragment_base_info_one){
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initTransFragmentEvent()
+
+        (activity as SignUpActivity).binding.pbSignup.progress = 50
+    }
+
+    // Fragment 초기화
+    private fun initTransFragmentEvent() {
+        binding.btnNext.setOnClickListener {
+            val baseInfoTwoFragment = BaseInfoTwoFragment()
+
+            parentFragmentManager.beginTransaction().replace(R.id.fragment_sign_up, baseInfoTwoFragment)
+                .commit()
+        }
+    }
 }
