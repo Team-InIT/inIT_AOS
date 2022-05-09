ef4450c1f8974f4dfd5ae3425b45358e13d46bec
lhb8106 <lhb8106@naver.com>
2022-05-09 15:27:49 +0900

    [FEAT] #8 - add server setting

---
+++ app/src/main/java/com/init_android/app/ServiceCreator.kt
@@ -0,0 +1,8 @@
+package com.init_android.app
+
+object ServiceCreator {
+    private const val BASE_URL = "http://192.168.0.12:3006/"
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .
+}
