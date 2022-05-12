a60136767ac0ab1f45ff26ee02ed4927b9a91dc8
lhb8106 <lhb8106@naver.com>
2022-05-13 02:06:57 +0900

    [FEAT] #3 - project view

---
+++ app/src/main/java/com/init_android/app/presentation/ui/OpenProjectActivity.kt
@@ -0,0 +1,12 @@
+package com.init_android.app.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.init_android.R
+
+class OpenProjectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_project)
+    }
+}
