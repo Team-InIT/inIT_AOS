0b43275e6d9d980994f1b0bdb48f3e07683ee617
stopkite <lilyandcat@swu.ac.kr>
2022-05-24 16:26:35 +0900

    [MODIFY] #30 - modify bottom navi setting

--- app/src/main/java/com/init_android/app/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/init_android/app/presentation/ui/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.init_android.app.presentation.ui
 
-import android.content.Intent
 import android.os.Bundle
 import com.init_android.R
 import com.init_android.app.presentation.ui.alarm.AlarmFragment
+import com.init_android.app.presentation.ui.feed.FeedFragment
 import com.init_android.app.presentation.ui.home.HomeFragment
 import com.init_android.app.presentation.ui.mypage.MyPageFragment
-import com.init_android.app.presentation.ui.oepn.project.OpenProjectActivity
 import com.init_android.app.presentation.ui.search.SearchFragment
 import com.init_android.databinding.ActivityMainBinding
 import com.playtogether_android.app.presentation.base.BaseActivity
@@ -16,7 +15,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initBottomNavi()
-        settingId()
+        //settingId()
     }
 
     private fun initBottomNavi() {
@@ -32,6 +31,10 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.fragment_main, SearchFragment()).commit()
                 }
+                R.id.menu_feed -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_main,FeedFragment()).commit()
+                }
                 R.id.menu_alarm -> {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.fragment_main, AlarmFragment()).commit()
@@ -46,12 +49,12 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     }
 
     //userId저장
-    private fun settingId() {
+  /*  private fun settingId() {
         val userId = intent.getIntExtra("userId", 0)
         binding.fabWriting.setOnClickListener {
             val intent = Intent(this@MainActivity, OpenProjectActivity::class.java)
             intent.putExtra("userId", userId)
             startActivity(intent)
         }
-    }
+    }*/
 }
---
+++ app/src/main/java/com/init_android/app/presentation/ui/feed/FeedFragment.kt
@@ -0,0 +1,9 @@
+package com.init_android.app.presentation.ui.feed
+
+import com.init_android.R
+import com.init_android.databinding.FragmentFeedBinding
+import com.playtogether_android.app.presentation.base.BaseFragment
+
+class FeedFragment : BaseFragment<FragmentFeedBinding>(R.layout.fragment_feed) {
+
+}
