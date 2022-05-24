dac617e2332e8e5404737567877efc1af6763fee
stopkite <lilyandcat@swu.ac.kr>
2022-05-24 20:39:43 +0900

    [FEAT] #32 - add date and string convert code

--- app/src/main/java/com/init_android/app/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/init_android/app/presentation/ui/home/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.init_android.app.presentation.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.init_android.R
+import com.init_android.app.util.DateUtil
 import com.init_android.app.util.PreferenceUtil
 import com.init_android.databinding.FragmentHomeBinding
 import com.playtogether_android.app.presentation.base.BaseFragment
@@ -13,12 +15,11 @@ import com.playtogether_android.app.presentation.base.BaseFragment
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+            val date = DateUtil().stringToDate(binding.tvHoho.text.toString())
+            val text = DateUtil().dateToString(date)
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+        Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
     }
 }
