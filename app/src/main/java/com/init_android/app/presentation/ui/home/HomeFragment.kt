2b34d6e71bf858ed949034b14cb2ee1f40daf7df
stopkite <lilyandcat@swu.ac.kr>
2022-05-26 18:21:28 +0900

    [FEAT] #35 - add project item databinding code

--- app/src/main/java/com/init_android/app/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/init_android/app/presentation/ui/home/HomeFragment.kt
@@ -2,9 +2,7 @@ package com.init_android.app.presentation.ui.home
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import com.init_android.R
-import com.init_android.app.util.DateUtil
 import com.init_android.databinding.FragmentHomeBinding
 import com.playtogether_android.app.presentation.base.BaseFragment
 
@@ -14,5 +12,15 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
     }
+
+    // 데이터 존재 여부 검사
+
+    // 소속 프로젝트 초기화 - (viewpager)
+    private fun initIncludeProject(){
+
+    }
+
+    // 추천 프로젝트 초기화 - (recyclerview)
 }
