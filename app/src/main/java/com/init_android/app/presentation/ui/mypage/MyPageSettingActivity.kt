fe06ed8524b15c187e837c4bb94af45e86d2fd54
lhb8106 <lhb8106@naver.com>
2022-05-27 02:17:08 +0900

    [FEAT] #44 - modify setting activity code

--- app/src/main/java/com/init_android/app/presentation/ui/mypage/MyPageSettingActivity.kt
+++ app/src/main/java/com/init_android/app/presentation/ui/mypage/MyPageSettingActivity.kt
@@ -1,12 +1,14 @@
 package com.init_android.app.presentation.ui.mypage
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.init_android.R
+import com.init_android.databinding.ActivityMyPageSettingBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
 
-class MyPageSettingActivity : AppCompatActivity() {
+class MyPageSettingActivity : BaseActivity<ActivityMyPageSettingBinding>(R.layout.activity_my_page_setting) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_page_setting)
+
     }
 }
