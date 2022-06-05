196404bada04cf7bd477e25fa5400f0b22645864
kite <stopkite98@naver.com>
2022-06-05 13:56:11 +0900

    [UI] #80 - feed writing view

---
+++ app/src/main/java/com/init_android/app/presentation/ui/feed/write/FeedWritingActivity.kt
@@ -0,0 +1,8 @@
+package com.init_android.app.presentation.ui.feed.write
+
+import com.init_android.R
+import com.init_android.databinding.ActivityFeedWritingBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
+
+class FeedWritingActivity:BaseActivity<ActivityFeedWritingBinding>(R.layout.activity_feed_writing) {
+}
