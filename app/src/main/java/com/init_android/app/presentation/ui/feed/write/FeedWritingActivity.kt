00b8449d3c6679f9f6d53a1f1c08c67506f63c26
kite <stopkite98@naver.com>
2022-06-05 19:21:56 +0900

    [UI] #81 - add gallery img code

--- app/src/main/java/com/init_android/app/presentation/ui/feed/write/FeedWritingActivity.kt
+++ app/src/main/java/com/init_android/app/presentation/ui/feed/write/FeedWritingActivity.kt
@@ -1,16 +1,23 @@
 package com.init_android.app.presentation.ui.feed.write
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.init_android.R
 import com.init_android.databinding.ActivityFeedWritingBinding
 import com.playtogether_android.app.presentation.base.BaseActivity
 
 class FeedWritingActivity:BaseActivity<ActivityFeedWritingBinding>(R.layout.activity_feed_writing) {
+
+    private var getResult:ActivityResultLauncher<Intent> ?= null
 
     // 갤러리 접근 권한 런처
     private val requestPermissionLauncher =
@@ -19,6 +26,7 @@ class FeedWritingActivity:BaseActivity<ActivityFeedWritingBinding>(R.layout.acti
             isGranted:Boolean ->
             if (isGranted) { // 권한 획득 성공시
                 // 갤러리 열기
+                openGallery()
             } else { // 권한 획득 거부 시
                 Toast.makeText(this, "갤러리에 접근하기 위해서는 권한이 필요합니다.", Toast.LENGTH_SHORT).show()
             }
@@ -27,6 +35,7 @@ class FeedWritingActivity:BaseActivity<ActivityFeedWritingBinding>(R.layout.acti
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initPhotoPick()
+        getPhotoUri()
     }
 
     // 이미지 선택
@@ -44,11 +53,37 @@ class FeedWritingActivity:BaseActivity<ActivityFeedWritingBinding>(R.layout.acti
                 Manifest.permission.READ_EXTERNAL_STORAGE
             ) == PackageManager.PERMISSION_GRANTED -> {  // 이미 권한부여를 받았기에 권한이 필요한 작업 수행
                 // 갤러리 열기
+                openGallery()
             }
 
             else -> {
                 requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)   // 바로 권한요청하는 경우
             }
         }
+    }
+
+    // 갤러리 열기
+    private fun openGallery(){
+        val profileIntent = Intent()
+        profileIntent.type = "image/*"
+        profileIntent.action = Intent.ACTION_GET_CONTENT
+        getResult?.launch(profileIntent)
+    }
+
+    // 이미지 받아오기
+    private fun getPhotoUri(){
+        getResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == Activity.RESULT_OK) {
+                    try {
+                        val uri: Uri? = (it.data)?.data
+                        Glide.with(this).load(uri).into(binding.ivProjectImg)
+                    } catch (e: Exception) {
+
+                    }
+                } else if (it.resultCode == Activity.RESULT_CANCELED) {
+                    Toast.makeText(this, "선택 취소", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
