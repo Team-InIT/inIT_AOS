7f220edb79468961b09808887b38be9d6494b300
lhb8106 <lhb8106@naver.com>
2022-06-05 19:15:39 +0900

    [FEAT] #68 - open project server connection

---
+++ app/src/main/java/com/init_android/app/data/request/mypage/RequestCountProject.kt
@@ -0,0 +1,5 @@
+package com.init_android.app.data.request.mypage
+
+data class RequestCountProject(
+    val mNum: Int
+)
---
+++ app/src/main/java/com/init_android/app/data/response/mypage/ResponseCountProject.kt
@@ -0,0 +1,9 @@
+package com.init_android.app.data.response.mypage
+
+data class ResponseCountProject(
+    val code: Int,
+    val disapproval: Int,
+    val join: Int,
+    val upload: Int,
+    val zzim: Int
+)
