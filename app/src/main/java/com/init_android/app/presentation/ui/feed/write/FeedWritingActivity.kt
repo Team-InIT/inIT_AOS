dd39d35e9f62012200c24e55ccec9369469a5100
kite <stopkite98@naver.com>
2022-06-05 18:06:33 +0900

    [UI] #81 - add gallery request permission code

--- app/src/main/java/com/init_android/app/presentation/ui/feed/write/FeedWritingActivity.kt
+++ app/src/main/java/com/init_android/app/presentation/ui/feed/write/FeedWritingActivity.kt
@@ -1,8 +1,54 @@
 package com.init_android.app.presentation.ui.feed.write
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.init_android.R
 import com.init_android.databinding.ActivityFeedWritingBinding
 import com.playtogether_android.app.presentation.base.BaseActivity
 
 class FeedWritingActivity:BaseActivity<ActivityFeedWritingBinding>(R.layout.activity_feed_writing) {
+
+    // 갤러리 접근 권한 런처
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission())
+        {
+            isGranted:Boolean ->
+            if (isGranted) { // 권한 획득 성공시
+                // 갤러리 열기
+            } else { // 권한 획득 거부 시
+                Toast.makeText(this, "갤러리에 접근하기 위해서는 권한이 필요합니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initPhotoPick()
+    }
+
+    // 이미지 선택
+    private fun initPhotoPick(){
+        binding.cardViewSelectPhoto.setOnClickListener {
+            requestPermission()
+        }
+    }
+
+    // 권한 체크 함수
+    private fun requestPermission(){
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {  // 이미 권한부여를 받았기에 권한이 필요한 작업 수행
+                // 갤러리 열기
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)   // 바로 권한요청하는 경우
+            }
+        }
+    }
 }
