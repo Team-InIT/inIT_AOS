58a543cf58896d80ef081fa61113bd379ce25b17
lhb8106 <lhb8106@naver.com>
2022-06-06 15:04:49 +0900

    [UI] #92 - main view end

---
+++ app/src/main/java/com/init_android/app/presentation/ui/open/partner/PartnerCheckActivity.kt
@@ -0,0 +1,12 @@
+package com.init_android.app.presentation.ui.open.partner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.init_android.R
+
+class PartnerCheckActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_partner_check)
+    }
+}
