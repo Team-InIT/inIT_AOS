8d234218c9ad3c0aceae30f251bce9c3b5d83129
lhb8106 <lhb8106@naver.com>
2022-06-06 15:38:03 +0900

    [FEAT] #92 - fragment view end

--- app/src/main/java/com/init_android/app/presentation/ui/open/partner/PartnerCheckActivity.kt
+++ app/src/main/java/com/init_android/app/presentation/ui/open/partner/PartnerCheckActivity.kt
@@ -3,10 +3,12 @@ package com.init_android.app.presentation.ui.open.partner
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.init_android.R
+import com.init_android.databinding.ActivityPartnerCheckBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
 
-class PartnerCheckActivity : AppCompatActivity() {
+class PartnerCheckActivity : BaseActivity<ActivityPartnerCheckBinding>(R.layout.activity_partner_check) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_partner_check)
+
     }
 }
