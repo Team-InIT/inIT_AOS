603a260cb7e78aae354dff914bcabf9598b659d2
lhb8106 <lhb8106@naver.com>
2022-06-08 17:41:01 +0900

    [FEAT] #104 - write server connection check

--- app/src/main/java/com/init_android/app/data/ServiceCreator.kt
+++ app/src/main/java/com/init_android/app/data/ServiceCreator.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceCreator {
-    private const val BASE_URL = "http://192.168.0.5:3006/"
+    private const val BASE_URL = "http://192.168.0.17:3006/"
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
