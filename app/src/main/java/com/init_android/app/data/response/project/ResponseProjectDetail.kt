6bef3fd618941b9a7ca6af80bdd6d76d82d333e4
lhb8106 <lhb8106@naver.com>
2022-06-08 16:37:39 +0900

    [FEAT] #99 - modify detail project activity

--- app/src/main/java/com/init_android/app/data/response/project/ResponseProjectDetail.kt
+++ app/src/main/java/com/init_android/app/data/response/project/ResponseProjectDetail.kt
@@ -7,39 +7,39 @@ data class ResponseProjectDetail(
 ) {
     data class ProjectInfo(
         val mNum: Int,
-        val pAcademic: Any,
+        val pAcademic: Int?,
         val pAos: Int,
-        val pAosf: Int,
+        val pAosf: Int?,
         val pDescription: String,
         val pDesign: Int,
-        val pDesignf: Any,
+        val pDesignf: Int?,
         val pDue: String,
         val pGame: Int,
-        val pGamef: Any,
-        val pGender: Any,
+        val pGamef: Int?,
+        val pGender: Int?,
         val pIos: Int,
-        val pIosf: Any,
+        val pIosf: Int?,
         val pNum: Int,
         val pOnOff: Int,
         val pPlan: Int,
-        val pPlanf: Any,
+        val pPlanf: Int?,
         val pRecruitDue: String,
         val pRecruitStart: String,
         val pServer: Int,
-        val pServerf: Int,
+        val pServerf: Int?,
         val pStack: List<String>,
         val pStart: String,
         val pState: Int,
         val pTitle: String,
         val pType: Int,
         val pWeb: Int,
-        val pWebf: Any
+        val pWebf: Int?
     )
 
     data class WriterInfo(
         val mName: String,
         val mNum: Int,
-        val mPhoto: Any,
+        val mPhoto: Any?,
         val mPosition: Int
     )
 }
