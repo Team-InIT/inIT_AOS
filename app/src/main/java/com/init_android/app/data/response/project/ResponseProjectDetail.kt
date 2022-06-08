924077c3c21b6e2b158ec25aab616a3b0dfcfa8d
lhb8106 <lhb8106@naver.com>
2022-06-08 17:59:24 +0900

    [FEAT] #104 - server connection check end

--- app/src/main/java/com/init_android/app/data/response/project/ResponseProjectDetail.kt
+++ app/src/main/java/com/init_android/app/data/response/project/ResponseProjectDetail.kt
@@ -2,44 +2,44 @@ package com.init_android.app.data.response.project
 
 data class ResponseProjectDetail(
     val code: Int,
-    val projectInfo: ProjectInfo,
-    val writerInfo: WriterInfo
+    val projectInfo: ProjectInfo?,
+    val writerInfo: WriterInfo?
 ) {
     data class ProjectInfo(
-        val mNum: Int,
+        val mNum: Int?,
         val pAcademic: Int?,
-        val pAos: Int,
+        val pAos: Int?,
         val pAosf: Int?,
-        val pDescription: String,
-        val pDesign: Int,
+        val pDescription: String?,
+        val pDesign: Int?,
         val pDesignf: Int?,
-        val pDue: String,
-        val pGame: Int,
+        val pDue: String?,
+        val pGame: Int?,
         val pGamef: Int?,
         val pGender: Int?,
-        val pIos: Int,
+        val pIos: Int?,
         val pIosf: Int?,
-        val pNum: Int,
-        val pOnOff: Int,
-        val pPlan: Int,
+        val pNum: Int?,
+        val pOnOff: Int?,
+        val pPlan: Int?,
         val pPlanf: Int?,
-        val pRecruitDue: String,
-        val pRecruitStart: String,
-        val pServer: Int,
+        val pRecruitDue: String?,
+        val pRecruitStart: String?,
+        val pServer: Int?,
         val pServerf: Int?,
-        val pStack: List<String>,
-        val pStart: String,
-        val pState: Int,
-        val pTitle: String,
-        val pType: Int,
-        val pWeb: Int,
+        val pStack: List<String>?,
+        val pStart: String?,
+        val pState: Int?,
+        val pTitle: String?,
+        val pType: Int?,
+        val pWeb: Int?,
         val pWebf: Int?
     )
 
     data class WriterInfo(
-        val mName: String,
-        val mNum: Int,
+        val mName: String?,
+        val mNum: Int?,
         val mPhoto: Any?,
-        val mPosition: Int
+        val mPosition: Int?
     )
 }
