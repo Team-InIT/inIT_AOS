7545e3addbddefb3269e007274d534bf5364dd7e
lhb8106 <lhb8106@naver.com>
2022-06-10 01:38:37 +0900

    [UI] #115 - mypage project detail view

---
+++ app/src/main/java/com/init_android/app/presentation/ui/mypage/MyPageReadyActivity.kt
@@ -0,0 +1,12 @@
+package com.init_android.app.presentation.ui.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.init_android.R
+
+class MyPageReadyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page_ready)
+    }
+}
