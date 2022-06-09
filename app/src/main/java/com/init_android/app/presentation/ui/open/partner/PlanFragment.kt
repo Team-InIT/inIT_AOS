10773aabfb72c14cb9ea27d6bf4c6f6c04a2f5b5
lhb8106 <lhb8106@naver.com>
2022-06-10 06:26:00 +0900

    [FEAT] #122 - add recyclerview

--- app/src/main/java/com/init_android/app/presentation/ui/open/partner/PlanFragment.kt
+++ app/src/main/java/com/init_android/app/presentation/ui/open/partner/PlanFragment.kt
@@ -5,14 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
 import com.init_android.R
+import com.init_android.app.data.request.project.RequestProjectMember
+import com.init_android.app.data.response.project.ResponseProjectMember
+import com.init_android.app.presentation.ui.open.partner.adapter.PartnerAdapter
+import com.init_android.app.presentation.ui.open.viewmodel.ProjectViewModel
 import com.init_android.databinding.FragmentPlanBinding
 import com.playtogether_android.app.presentation.base.BaseFragment
 
 
 class PlanFragment : BaseFragment<FragmentPlanBinding>(R.layout.fragment_plan) {
+
+    private val projectViewModel: ProjectViewModel by viewModels()
+    private lateinit var partnerAdapter: PartnerAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initNetwork()
+
+    }
+
+    private fun initNetwork() {
+        val requestProjectMember = RequestProjectMember(pNum = 1)
+        projectViewModel.postProjectMember(requestProjectMember)
+        partnerAdapter = PartnerAdapter(1)
+        projectViewModel.projectMember.observe(this) {
+            partnerAdapter.setQuestionPost((it.approvedPlan) as MutableList<ResponseProjectMember.ApprovedPlan>)
+        }
     }
 }
