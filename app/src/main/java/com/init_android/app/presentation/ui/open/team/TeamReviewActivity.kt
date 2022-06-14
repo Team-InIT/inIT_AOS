847c8f0dd08ad6c8edf4988cbfe607950e06387a
kite <stopkite98@naver.com>
2022-06-14 22:22:32 +0900

    [UI] #148 - add team review activity layout

---
+++ app/src/main/java/com/init_android/app/presentation/ui/open/team/TeamReviewActivity.kt
@@ -0,0 +1,9 @@
+package com.init_android.app.presentation.ui.open.team
+
+import com.init_android.R
+import com.init_android.databinding.ActivityTeamReviewBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
+
+class TeamReviewActivity:BaseActivity<ActivityTeamReviewBinding>(R.layout.activity_team_review) {
+
+}
