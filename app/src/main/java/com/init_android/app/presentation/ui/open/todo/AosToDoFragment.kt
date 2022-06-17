e2ecf156c093dddf370917d1082ed49781d800f4
lhb8106 <lhb8106@naver.com>
2022-06-18 03:18:43 +0900

    [FEAT] #171 - to do server connection item list end

--- app/src/main/java/com/init_android/app/presentation/ui/open/todo/AosToDoFragment.kt
+++ app/src/main/java/com/init_android/app/presentation/ui/open/todo/AosToDoFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.init_android.R
+import com.init_android.app.data.request.project.RequestProjectMember
+import com.init_android.app.data.response.todo.ResponseAllToDo
+import com.init_android.app.presentation.ui.open.todo.adapter.ToDoPlanAdapter
+import com.init_android.app.presentation.ui.open.todo.viewmodel.ToDoViewModel
 import com.init_android.databinding.FragmentAosToDoBinding
 import com.init_android.databinding.FragmentPlanToDoBinding
 import com.playtogether_android.app.presentation.base.BaseFragment
@@ -13,8 +18,25 @@ import com.playtogether_android.app.presentation.base.BaseFragment
 
 class AosToDoFragment : BaseFragment<FragmentAosToDoBinding>(R.layout.fragment_aos_to_do) {
 
+    private val todoViewModel : ToDoViewModel by viewModels()
+    private lateinit var toDoPlanAdapter: ToDoPlanAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initNetwork()
+    }
+
+    private fun initNetwork() {
+        val requestProjectMember = RequestProjectMember(pNum = 1)
+        todoViewModel.postReadAosToDo(requestProjectMember)
+        toDoPlanAdapter = ToDoPlanAdapter()
+        binding.rvAos.adapter = toDoPlanAdapter
+        todoViewModel.readAllToDo.observe(viewLifecycleOwner) {
+            toDoPlanAdapter.setQuestionPost((it.todoList) as MutableList<ResponseAllToDo.Todo>)
+        }
     }
 }
