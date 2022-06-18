a304df48bd88ee2f8edf315c2f9be6e48db7f03e
lhb8106 <lhb8106@naver.com>
2022-06-18 23:37:21 +0900

    [FEAT] #184 - evaluation sever connection

--- app/src/main/java/com/init_android/app/presentation/ui/mypage/MyPageRateFragment.kt
+++ app/src/main/java/com/init_android/app/presentation/ui/mypage/MyPageRateFragment.kt
@@ -1,37 +1,41 @@
 package com.init_android.app.presentation.ui.mypage
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.init_android.R
+import com.init_android.app.data.request.mypage.RequestWaitingApproval
+import com.init_android.app.data.request.project.RequestProjectMember
 import com.init_android.app.data.response.ResponseRateData
+import com.init_android.app.data.response.mypage.ResponseEvaluation
+import com.init_android.app.data.response.project.approve.ResponseProjectMember
 import com.init_android.app.presentation.ui.mypage.adapter.RatingAdapter
+import com.init_android.app.presentation.ui.mypage.viewmodel.MyPageViewModel
+import com.init_android.app.presentation.ui.open.partner.adapter.PartnerAosAdapter
 import com.init_android.databinding.FragmentMyPageRateBinding
 import com.playtogether_android.app.presentation.base.BaseFragment
 
 class MyPageRateFragment : BaseFragment<FragmentMyPageRateBinding>(R.layout.fragment_my_page_rate) {
     private lateinit var ratingAdapter: RatingAdapter
+    private val myPageViewModel : MyPageViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initAdapter()
+        initNetwork()
     }
 
-    private fun initAdapter() {
+    private fun initNetwork() {
+        val requestWaitingApproval = RequestWaitingApproval(mNum = 1)
+        myPageViewModel.postEvalMem(requestWaitingApproval)
         ratingAdapter = RatingAdapter()
         binding.rvRating.adapter = ratingAdapter
-
-        ratingAdapter.rateList.addAll(
-            listOf(
-                ResponseRateData(R.drawable.ic_launcher_background,"이름","안드로이드 프로젝트", "성실하고 좋은 팀원이었습니다"),
-                ResponseRateData(R.drawable.ic_launcher_background,"이름","안드로이드 프로젝트", "성실하고 좋은 팀원이었습니다"),
-                ResponseRateData(R.drawable.ic_launcher_background,"이름","안드로이드 프로젝트", "성실하고 좋은 팀원이었습니다")
-            )
-        )
-
-        ratingAdapter.notifyDataSetChanged()
+        myPageViewModel.myEvaluation.observe(viewLifecycleOwner) {
+            ratingAdapter.setQuestionPost((listOf(it)) as MutableList<ResponseEvaluation.MyEvaluation>)
+        }
     }
 
 
