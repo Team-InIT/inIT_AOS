6703dd6f9a1b0c1e07ba5698603c66883680bca6
lhb8106 <lhb8106@naver.com>
2022-06-18 20:36:43 +0900

    [FEAT] #182 - modify data source

--- app/src/main/java/com/init_android/app/data/response/project/ResponseProjectDetail.kt
+++ app/src/main/java/com/init_android/app/data/response/project/ResponseProjectDetail.kt
@@ -1,46 +1,51 @@
 package com.init_android.app.data.response.project
 
 data class ResponseProjectDetail(
-    val code: Int,
-    val isApproval: Boolean?,
-    val projectInfo: ProjectInfo?,
-    val writerInfo: WriterInfo?
+    val code: Int?,
+    val detailInfo: DetailInfo?,
+    val isApproval: Boolean?
 ) {
-    data class ProjectInfo(
-        val mNum: Int?,
-        val pAcademic: Int?,
-        val pAos: Int?,
-        val pAosf: Int?,
-        val pDescription: String?,
-        val pDesign: Int?,
-        val pDesignf: Int?,
-        val pDue: String?,
-        val pGame: Int?,
-        val pGamef: Int?,
-        val pGender: Int?,
-        val pIos: Int?,
-        val pIosf: Int?,
-        val pNum: Int?,
-        val pOnOff: Int?,
-        val pPlan: Int?,
-        val pPlanf: Int?,
-        val pRecruitDue: String?,
-        val pRecruitStart: String?,
-        val pServer: Int?,
-        val pServerf: Int?,
-        val pStack: List<String>?,
-        val pStart: String?,
-        val pState: Int?,
-        val pTitle: String?,
-        val pType: Int?,
-        val pWeb: Int?,
-        val pWebf: Int?
-    )
+    data class DetailInfo(
+        val projectInfo: ProjectInfo?,
+        val projectState: String?,
+        val writerInfo: WriterInfo?
+    ) {
+        data class ProjectInfo(
+            val mNum: Int?,
+            val pAcademic: Int?,
+            val pAos: Int?,
+            val pAosf: Int?,
+            val pDescription: String?,
+            val pDesign: Int?,
+            val pDesignf: Int?,
+            val pDue: String?,
+            val pGame: Int?,
+            val pGamef: Int?,
+            val pGender: Int?,
+            val pIos: Int?,
+            val pIosf: Int?,
+            val pNum: Int?,
+            val pOnOff: Int?,
+            val pPlan: Int?,
+            val pPlanf: Int?,
+            val pRecruitDue: String?,
+            val pRecruitStart: String?,
+            val pServer: Int?,
+            val pServerf: Int?,
+            val pStack: Int?,
+            val pStart: String?,
+            val pState: Int?,
+            val pTitle: String?,
+            val pType: Int?,
+            val pWeb: Int?,
+            val pWebf: Int?
+        )
 
-    data class WriterInfo(
-        val mName: String?,
-        val mNum: Int?,
-        val mPhoto: String?,
-        val mPosition: Int?
-    )
+        data class WriterInfo(
+            val mName: String?,
+            val mNum: Int?,
+            val mPhoto: String?,
+            val mPosition: Int?
+        )
+    }
 }
